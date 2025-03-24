ab68f6533096c747a7c8530b73045593e6367885
endergreen12 <endergreenjapan@gmail.com>
2025-03-25 04:31:51 +0900

    Comment out interesting function implementations

--- misc.ts
+++ misc.ts
@@ -20,28 +20,28 @@ AssemblyImage.class("GooglePlayGames.PlayGamesPlatform").method("Authenticate")
 }
 
 // Override interesting function implementations //
-    AssemblyImage.class("Sekai.Core.Live.ScoreLogic").method("Damage").implementation = function(noteInfo: Il2Cpp.Object)
+    /* AssemblyImage.class("Sekai.Core.Live.ScoreLogic").method("Damage").implementation = function(noteInfo: Il2Cpp.Object)
     {
         this.method("Damage").invoke(noteInfo)
-    }
+    } */
 
-    AssemblyImage.class("Sekai.Live.LiveConfig").method<Il2Cpp.Object>("GetJudgeResultInternal").implementation = function(offset: number, judgeFrameType: number, isDirection: boolean)
+    /* AssemblyImage.class("Sekai.Live.LiveConfig").method<Il2Cpp.Object>("GetJudgeResultInternal").implementation = function(offset: number, judgeFrameType: number, isDirection: boolean)
     {
         return this.method<Il2Cpp.Object>("GetJudgeResultInternal").invoke(offset, judgeFrameType, isDirection)
-    }
+    } */
 
-    AssemblyImage.class("Sekai.Core.Live.LiveLogic").method<number>("CalcNoteSpeedRatio").implementation = function(currentProgress: number, noteProgress: number)
+    /* AssemblyImage.class("Sekai.Core.Live.LiveLogic").method<number>("CalcNoteSpeedRatio").implementation = function(currentProgress: number, noteProgress: number)
     {
         return this.method<number>("CalcNoteSpeedRatio").invoke(currentProgress, noteProgress)
-    }
+    } */
 
-    AssemblyImage.class("Sekai.Live.MusicInfoView").method("Play").implementation = function(durationScale: number, disableOnlyJacketOnFinish: boolean, bgFadeoutDuration: number)
+    /* AssemblyImage.class("Sekai.Live.MusicInfoView").method("Play").implementation = function(durationScale: number, disableOnlyJacketOnFinish: boolean, bgFadeoutDuration: number)
     {
         this.method("Play").invoke(durationScale, disableOnlyJacketOnFinish, bgFadeoutDuration)
-    }
+    } */
 
-    AssemblyImage.class("Sekai.SUS.Converter").method<Il2Cpp.Object>("Convert").implementation = function(musicScoreData: Il2Cpp.String)
+    /* AssemblyImage.class("Sekai.SUS.Converter").method<Il2Cpp.Object>("Convert").implementation = function(musicScoreData: Il2Cpp.String)
     {
         const scoreData = this.method<Il2Cpp.Object>("Convert").invoke(musicScoreData)
         return scoreData
-    }
+    } */
