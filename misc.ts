f2fd80b9f4d1a5cf279c079d78b025c3e68d3f07
endergreen12 <endergreenjapan@gmail.com>
2025-04-28 22:38:06 +0900

    playgrounds: fake gacha

--- misc.ts
+++ misc.ts
@@ -29,4 +29,23 @@ Il2Cpp.perform(() => {
         category.value = AssemblyImage.class("Sekai.Live.NoteCategory").field("Flick").value
         this.method("ConvertFlickNote").invoke(id, noteInfo, info, laneStart, laneEnd, category)
     }
+
+    // Fake Gacha
+    AssemblyImage.class("Sekai.ScreenLayerHome").method("OnClickGacha").implementation = function()
+    {
+        const response = AssemblyImage.class("Sekai.UserGachaResponse").new()
+        response.field("consumedCosts").value = Il2Cpp.array(AssemblyImage.class("Sekai.UserResource"), 0)
+        response.field("obtainGachaBonusItems").value = Il2Cpp.array(AssemblyImage.class("Sekai.UserResource"), 0)
+        response.field("obtainGachaCeilItems").value = Il2Cpp.array(AssemblyImage.class("Sekai.UserResource"), 0)
+        response.field("obtainGachaExtras").value = Il2Cpp.array(AssemblyImage.class("Sekai.UserResource"), 0)
+        response.field("obtainGachaFreebies").value = Il2Cpp.array(AssemblyImage.class("Sekai.UserGachaFreebie"), 0)
+        response.field("obtainPrizes").value = Il2Cpp.array(AssemblyImage.class("Sekai.UserGachaSpinObtainPrize"), 0)
+        response.field("updatedResources").value = AssemblyImage.class("Sekai.SuiteUser").new()
+        response.field("userGacha").value = AssemblyImage.class("Sekai.UserGacha").new()
+
+        const masterGacha = AssemblyImage.class("Sekai.GachaUtility").method<Il2Cpp.Object>("GetMasterGachaOf").invoke(593)
+        const behavior = masterGacha.field<Il2Cpp.Array<Il2Cpp.Object>>("gachaBehaviors").value.get(0)
+
+        AssemblyImage.class("Sekai.GachaUtility").method("TransitionSpinAnimation").invoke(response, masterGacha, behavior)
+    }
 })
