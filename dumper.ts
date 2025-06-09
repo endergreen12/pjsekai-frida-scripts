c4bcb495d6f2a780cb5a54042ce0e1c5fe868daf
endergreen12 <endergreenjapan@gmail.com>
2025-06-09 22:45:06 +0900

    dumper: Add button and dump wording

--- dumper.ts
+++ dumper.ts
@@ -1,27 +1,41 @@
-import { AssemblyImage } from "./lib/consts.js";
+import { AssemblyImage, CoreModuleImage } from "./lib/consts.js";
+import { CreateButton } from "./lib/lib.js";
 const DUMP_DIR_PATH = "/sdcard/pjsekai_dumps"
 
 Il2Cpp.perform(() => {
-    AssemblyImage.class("Sekai.ScreenLayerHome").method("OnClickJoinMysekai").implementation = function()
+    AssemblyImage.class("Sekai.OptionDialog").method("Setup").implementation = function(tabIndex, canAssetSetting, canBlockListSetting)
     {
-        const masterDataManager = AssemblyImage.class("Sekai.MasterDataManager").method<Il2Cpp.Object>("get_Instance").invoke()
+        this.method("Setup").invoke(tabIndex, canAssetSetting, canBlockListSetting)
 
-        console.log("Creating a folder...")
-        Il2Cpp.corlib.class("System.IO.Directory").method("CreateDirectory").invoke(Il2Cpp.string(DUMP_DIR_PATH))
+        const parentTransform = this.method<Il2Cpp.Object>("GetComponent", 0).inflate(CoreModuleImage.class("UnityEngine.RectTransform")).invoke()
+        CreateButton(3, 770, 150, 300, 100, 38, parentTransform, (button: Il2Cpp.Object) => {
+            const masterDataManager = AssemblyImage.class("Sekai.MasterDataManager").method<Il2Cpp.Object>("get_Instance").invoke()
 
-        console.log("Beginning dumping...")
-        const targetDataNameArray: string[] = ["MasterMusicAlls", "MasterEventStories", "MasterSpecialStoryMap", "MasterUnitStoryEpisodeAll"]
-        targetDataNameArray.forEach((targetDataName) => dump(targetDataName, `${targetDataName}.json`, masterDataManager))
+            console.log("Creating a folder...")
+            Il2Cpp.corlib.class("System.IO.Directory").method("CreateDirectory").invoke(Il2Cpp.string(DUMP_DIR_PATH))
 
-        console.log("All dumps are complete!")
+            console.log("Beginning dumping of MasterData...")
+            const targetDataNameArray: string[] = ["MasterMusicAlls", "MasterEventStories", "MasterSpecialStoryMap", "MasterUnitStoryEpisodeAll"]
+            targetDataNameArray.forEach((targetDataName) => dumpMasterData(targetDataName, `${targetDataName}.json`, masterDataManager))
+
+            console.log("Dumping wording...")
+            serializeAndWriteToFile(AssemblyImage.class("Sekai.WordingManager").field<Il2Cpp.Object>("dictionary").value, "WordingDictionary.json")
+
+            console.log("All dumps are complete!")
+        }, "Start dumping")
     }
 })
 
-function dump(targetDataName: string, dumpFileName: string, masterDataManager: Il2Cpp.Object)
+function dumpMasterData(targetDataName: string, dumpFileName: string, masterDataManager: Il2Cpp.Object)
 {
     console.log(`Starting dumping of ${targetDataName}...`)
 
     const targetData = masterDataManager.method<Il2Cpp.Object>(`Get${targetDataName}`).invoke()
+    serializeAndWriteToFile(targetData, dumpFileName)
+}
+
+function serializeAndWriteToFile(targetData: Il2Cpp.Object, dumpFileName: string)
+{
     const jsonSerializedData = AssemblyImage.class("CP.JsonSerializer").method<Il2Cpp.String>("ToJsonWithUnicodeDecode").invoke(targetData)
 
     console.log("Writing dumped data to a file...")
@@ -29,4 +43,5 @@ function dump(targetDataName: string, dumpFileName: string, masterDataManager: I
         .invoke(Il2Cpp.string(DUMP_DIR_PATH + "/" + dumpFileName), jsonSerializedData)
 
     console.log("Done")
+    console.log()
 }
