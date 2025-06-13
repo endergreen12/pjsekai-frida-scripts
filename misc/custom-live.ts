6ec480608bb86fda8ed1097e214eb4772a2fd7f4
endergreen12 <endergreenjapan@gmail.com>
2025-06-13 19:23:59 +0900

    custom-live: Create a dedicated button

--- misc/custom-live.ts
+++ misc/custom-live.ts
@@ -1,28 +1,47 @@
-import { AssemblyImage } from "../lib/consts.js"
-import { GetInstanceOfSingleton, SetProperty } from "../lib/lib.js"
+import { AssemblyImage, RectTransform } from "../lib/consts.js"
+import { CreateButton, GetComponentInChildrenFromObj, GetInstanceOfSingleton, SetProperty } from "../lib/lib.js"
 
 Il2Cpp.perform(() => {
-    AssemblyImage.class("Sekai.ScreenLayerHome").method("OnClickLive").implementation = function()
+    let isButtonCreated = false
+    AssemblyImage.class("Sekai.ScreenLayerLiveTop").method("OnInitComponent").implementation = function()
     {
-        // Set Sekai.Core.EntryPoint.PlayMode
-        SetProperty(AssemblyImage.class("Sekai.Core.EntryPoint"), "PlayMode", 3) // 3 = SoloLive
-        
-        // Set up liveBootData
-        const liveBootData = AssemblyImage.class("Sekai.FreeLiveBootData").alloc()
+        this.method("OnInitComponent").invoke()
 
-        const musicId = 578 // ハローセカイ (Hello Sekai)
-        const difficulty = Il2Cpp.string("master")
-        const vocalId = 1572 // バーチャル・シンガーver (Virtual Singer ver.)
-        const deckId = 1
+        if(isButtonCreated)
+        {
+            return
+        }
 
-        liveBootData.method(".ctor", 6).invoke(musicId, difficulty, vocalId, deckId, 0, false) // Call the constructor of Sekai.LiveBootDataBase
-        liveBootData.method(".ctor", 8).invoke(musicId, difficulty, vocalId, deckId, Il2Cpp.array(Il2Cpp.corlib.class("System.Int32"), 0), true, 0, 0) // Call the constructor of Sekai.FreeLiveBootData
-        liveBootData.method("SetLiveMode").invoke(3) // 3 = Low
-        SetProperty(liveBootData, "IsAuto", true)
+        CreateButton(2, -200, -80, 400, 100, 38, GetComponentInChildrenFromObj(this as Il2Cpp.Object, RectTransform), (button: Il2Cpp.Object) => {
+            // Set Sekai.Core.EntryPoint.PlayMode
+            SetProperty(AssemblyImage.class("Sekai.Core.EntryPoint"), "PlayMode", 3) // 3 = SoloLive
+            
+            // Set up liveBootData
+            const liveBootData = AssemblyImage.class("Sekai.FreeLiveBootData").alloc()
 
-        SetProperty(GetInstanceOfSingleton(AssemblyImage.class("Sekai.UserDataManager")), "FreeLiveBootData", liveBootData)
+            const musicId = 578 // ハローセカイ (Hello Sekai)
+            const difficulty = Il2Cpp.string("master")
+            const vocalId = 1572 // バーチャル・シンガーver (Virtual Singer ver.)
+            const deckId = 1
 
-        // Start live
-        GetInstanceOfSingleton(AssemblyImage.class("Sekai.ScreenManager")).method("AddScreen", 1).invoke(63) // 63 = LiveLoading
+            liveBootData.method(".ctor", 6).invoke(musicId, difficulty, vocalId, deckId, 0, false) // Call the constructor of Sekai.LiveBootDataBase
+            liveBootData.method(".ctor", 8).invoke(musicId, difficulty, vocalId, deckId, Il2Cpp.array(Il2Cpp.corlib.class("System.Int32"), 0), true, 0, 0) // Call the constructor of Sekai.FreeLiveBootData
+            liveBootData.method("SetLiveMode").invoke(3) // 3 = Low
+            SetProperty(liveBootData, "IsAuto", true)
+
+            SetProperty(GetInstanceOfSingleton(AssemblyImage.class("Sekai.UserDataManager")), "FreeLiveBootData", liveBootData)
+
+            // Start live
+            GetInstanceOfSingleton(AssemblyImage.class("Sekai.ScreenManager")).method("AddScreen", 1).invoke(63) // 63 = LiveLoading
+        }, "Start Live")
+
+        isButtonCreated = true
+    }
+
+    AssemblyImage.class("Sekai.ScreenLayerLiveTop").method(".ctor").implementation = function()
+    {
+        this.method(".ctor").invoke()
+
+        isButtonCreated = false
     }
 })
