45f894a7e856ce08fb3fe9ec046e9c795912cbe6
endergreen12 <endergreenjapan@gmail.com>
2025-06-13 19:41:37 +0900

    play-scenario, play-special-scenario: Create a dedicated button

--- misc/play-scenario.ts
+++ misc/play-scenario.ts
@@ -1,19 +1,39 @@
-import { AssemblyImage, SystemAction } from "../lib/consts.js";
+import { AssemblyImage, RectTransform, SystemAction } from "../lib/consts.js";
+import { CreateButton, GetComponentInChildrenFromObj } from "../lib/lib.js";
 
 Il2Cpp.perform(() => {
-    AssemblyImage.class("Sekai.ScreenLayerHome").method("OnClickStory").implementation = function()
+    let isButtonCreated = false
+    AssemblyImage.class("Sekai.ScreenLayerStoryCategorySelect").method("OnInitComponent").implementation = function()
     {
-        const assetBundleNamesClass = AssemblyImage.class("Sekai.AssetBundleNames")
+        this.method("OnInitComponent").invoke()
 
-        const assetbundleName = Il2Cpp.string("event_canvas_2022") // poor Ena
-        const scenarioAssetBundleName = assetBundleNamesClass.method<Il2Cpp.String>("GetEventStoryScenarioName").invoke(assetbundleName)
-        const voiceAssetBundleName = assetBundleNamesClass.method<Il2Cpp.String>("GetEventStoryVoiceBundleName").invoke(assetbundleName)
-        const scenarioId = Il2Cpp.string("event_53_02")
-        const episodeId = 1000424
+        if(isButtonCreated)
+        {
+            return
+        }
 
-        const onFinished = Il2Cpp.delegate(SystemAction, () => console.log("Scenario finished"))
+        CreateButton(2, -200, -80, 400, 100, 38, GetComponentInChildrenFromObj(this as Il2Cpp.Object, RectTransform), (button: Il2Cpp.Object) => {
+            const assetBundleNamesClass = AssemblyImage.class("Sekai.AssetBundleNames")
 
-        AssemblyImage.class("Sekai.ScenarioUtility").method("PlayScenario")
-            .invoke(scenarioAssetBundleName, voiceAssetBundleName, scenarioId, onFinished, NULL, NULL, episodeId, 0, NULL)
+            const assetbundleName = Il2Cpp.string("event_canvas_2022") // poor Ena
+            const scenarioAssetBundleName = assetBundleNamesClass.method<Il2Cpp.String>("GetEventStoryScenarioName").invoke(assetbundleName)
+            const voiceAssetBundleName = assetBundleNamesClass.method<Il2Cpp.String>("GetEventStoryVoiceBundleName").invoke(assetbundleName)
+            const scenarioId = Il2Cpp.string("event_53_02")
+            const episodeId = 1000424
+
+            const onFinished = Il2Cpp.delegate(SystemAction, () => console.log("Scenario finished"))
+
+            AssemblyImage.class("Sekai.ScenarioUtility").method("PlayScenario")
+                .invoke(scenarioAssetBundleName, voiceAssetBundleName, scenarioId, onFinished, NULL, NULL, episodeId, 0, NULL)
+        }, "Play Scenario")
+
+        isButtonCreated = true
+    }
+
+    AssemblyImage.class("Sekai.ScreenLayerStoryCategorySelect").method(".ctor").implementation = function()
+    {
+        this.method(".ctor").invoke()
+
+        isButtonCreated = false
     }
 })
--- misc/play-special-scenario.ts
+++ misc/play-special-scenario.ts
@@ -1,12 +1,31 @@
-import { AssemblyImage } from "../lib/consts.js";
-import { GetMasterDataManagerInstance } from "../lib/lib.js";
+import { AssemblyImage, RectTransform } from "../lib/consts.js";
+import { CreateButton, GetComponentInChildrenFromObj, GetMasterDataManagerInstance } from "../lib/lib.js";
 
 Il2Cpp.perform(() => {
-    AssemblyImage.class("Sekai.ScreenLayerHome").method("OnClickStory").implementation = function()
+    let isButtonCreated = false
+    AssemblyImage.class("Sekai.ScreenLayerStoryCategorySelect").method("OnInitComponent").implementation = function()
     {
-        const masterDataManager = GetMasterDataManagerInstance()
-        const specialStoryEpisode = masterDataManager.method<Il2Cpp.Object>("GetMasterSpecialStoryEpisode").invoke(8) // 一周年カウントダウンムービー01
+        this.method("OnInitComponent").invoke()
 
-        AssemblyImage.class("Sekai.ScenarioUtility").method("PlaySpecialStoryEpisode").invoke(specialStoryEpisode)
+        if(isButtonCreated)
+        {
+            return
+        }
+
+        CreateButton(2, -200, -80, 400, 100, 38, GetComponentInChildrenFromObj(this as Il2Cpp.Object, RectTransform), (button: Il2Cpp.Object) => {
+            const masterDataManager = GetMasterDataManagerInstance()
+            const specialStoryEpisode = masterDataManager.method<Il2Cpp.Object>("GetMasterSpecialStoryEpisode").invoke(56) // 一周年カウントダウンムービー01
+
+            AssemblyImage.class("Sekai.ScenarioUtility").method("PlaySpecialStoryEpisode").invoke(specialStoryEpisode)
+        }, "Play Special Scenario")
+
+        isButtonCreated = true
+    }
+
+    AssemblyImage.class("Sekai.ScreenLayerStoryCategorySelect").method(".ctor").implementation = function()
+    {
+        this.method(".ctor").invoke()
+
+        isButtonCreated = false
     }
 })
