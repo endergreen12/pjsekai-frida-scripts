4415b232959d121532b461cf40a85f326b930d6b
endergreen12 <endergreenjapan@gmail.com>
2025-08-14 00:26:17 +0900

    logger: Add trace for Sekai.ApplicationLogger

--- scripts/logger.ts
+++ scripts/logger.ts
@@ -1,6 +1,8 @@
 import "frida-il2cpp-bridge"
 import { GetCoreModuleImage } from "./lib/exports/get/unity"
+import { GetAssemblyCSharpImage } from "./lib/exports/get/assembly"
 
 Il2Cpp.perform(() => {
     Il2Cpp.trace(true).classes(GetCoreModuleImage().class("UnityEngine.Debug")).filterMethods(method => method.name.startsWith("Log")).and().attach()
+    Il2Cpp.trace(true).classes(GetAssemblyCSharpImage().class("Sekai.ApplicationLogger")).filterMethods(method => method.name === "WriteLog").and().attach()
 })
