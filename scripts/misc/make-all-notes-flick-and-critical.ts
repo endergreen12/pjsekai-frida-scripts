0b4c26d51eed7710b158ec2baae5764f74745293
endergreen12 <endergreenjapan@gmail.com>
2025-08-13 14:25:09 +0900

    changed a little bit

--- scripts/misc/make-all-notes-flick-and-critical.ts
+++ scripts/misc/make-all-notes-flick-and-critical.ts
@@ -1,8 +1,11 @@
 import "frida-il2cpp-bridge"
 import { GetAssemblyCSharpImage } from "../lib/exports/get/assembly"
 import { SetProperty, GetField } from "../lib/utils/unity/get-set"
+import { ChangeImpl_AutoRetire } from "./auto-retire"
 
 Il2Cpp.perform(() => {
+    ChangeImpl_AutoRetire()
+
     const AssemblyCSharpImage = GetAssemblyCSharpImage()
 
     AssemblyCSharpImage.class("Sekai.SUS.Converter").method("ConvertNormalNote").implementation = function(id, noteInfo: Il2Cpp.Reference<Il2Cpp.Object>, info, laneStart, laneEnd, category: Il2Cpp.Reference)
@@ -28,7 +31,6 @@ Il2Cpp.perform(() => {
     AssemblyCSharpImage.class("Sekai.SUS.Converter").method("ConvertFrictionFlickNote").implementation = function(id, noteInfo: Il2Cpp.Reference<Il2Cpp.Object>, info, laneStart, laneEnd, category: Il2Cpp.Reference)
     {
         SetProperty(noteInfo.value, "Type", 1)
-        category.value = GetField(AssemblyCSharpImage.class("Sekai.Live.NoteCategory"), "FrictionFlick")
         this.method("ConvertFrictionFlickNote").invoke(id, noteInfo, info, laneStart, laneEnd, category)
     }
 
