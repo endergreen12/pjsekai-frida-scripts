3df65a4278051c4652a220f5664e6dadc017c227
endergreen12 <endergreenjapan@gmail.com>
2025-08-19 16:23:43 +0900

    Fix dialog functions

--- scripts/lib/utils/game/dialog.ts
+++ scripts/lib/utils/game/dialog.ts
@@ -4,12 +4,16 @@ import { GetScreenManagerInstance } from "./instance"
 import { GetAssemblyCSharpImage } from "../../exports/get/assembly"
 import { IsEnglishVersion } from "./utils"
 
+/* 
+Note: In the English version, the order of dialogSize and layerType is reversed from the Japanese version, so I have swapped them around for the English version
+*/
+
 export const COMMON_1BUTTON_DIALOG_CLASS_NAME = "Sekai.Common1ButtonDialog"
 export function Show1ButtonDialog_1(className: string, dialogType: number, messageBodyKey: string, okButtonLabelKey: string, onClickOK: Il2Cpp.Object | NativePointer, layerType: number = DisplayLayerType.Layer_Dialog, dialogSize: number = DialogSize.Manual, allowCloseExternal: boolean = true): Il2Cpp.Object
 {
     return GetScreenManagerInstance()
         .method<Il2Cpp.Object>("Show1ButtonDialog", 7).inflate(GetAssemblyCSharpImage().class(className))
-        .invoke(dialogType, Il2Cpp.string(messageBodyKey), Il2Cpp.string(okButtonLabelKey), onClickOK, layerType, dialogSize, allowCloseExternal)
+        .invoke(dialogType, Il2Cpp.string(messageBodyKey), Il2Cpp.string(okButtonLabelKey), onClickOK, IsEnglishVersion() ? dialogSize : layerType, IsEnglishVersion() ? layerType : dialogSize, allowCloseExternal)
 }
 
 export function ShowSubWindowDialog(messageBody: string = null, onClose: Il2Cpp.Object | NativePointer = NULL, allowCloseExternal: boolean = true, dialogType: number = 240, layerType: number = DisplayLayerType.Layer_Dialog): Il2Cpp.Object
@@ -29,5 +33,5 @@ export function Show2ButtonDialog_1(className: string, dialogType: number, messa
 {
     return GetScreenManagerInstance()
         .method<Il2Cpp.Object>("Show2ButtonDialog", 9).inflate(GetAssemblyCSharpImage().class(className))
-        .invoke(dialogType, Il2Cpp.string(messageBodyKey), Il2Cpp.string(okButtonLabelKey), Il2Cpp.string(cancelButtonLabelKey), onClickOK, onClickCancel, layerType, dialogSize, allowCloseExternal)
+        .invoke(dialogType, Il2Cpp.string(messageBodyKey), Il2Cpp.string(okButtonLabelKey), Il2Cpp.string(cancelButtonLabelKey), onClickOK, onClickCancel, IsEnglishVersion() ? dialogSize : layerType, IsEnglishVersion() ? layerType : dialogSize, allowCloseExternal)
 }
